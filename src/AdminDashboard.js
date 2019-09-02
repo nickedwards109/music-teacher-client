403cd27f8bd42425b79f9d17ccd27d0a553086f1
nickedwards109 <nicholas.lee.edwards@gmail.com>
2019-09-02 18:20:45 -0400

    Show a user's name on their dashboard
    When a user visits either '/admin/dashboard' or '/student/dashboard', if they are authorized to view that dashboard, they see a welcome message with their name in it. This comes from sending a GET to the server at '/api/v1/:role/dashboard', which responds with JSON containing the user's name.

--- src/AdminDashboard.js
+++ src/AdminDashboard.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
 
 class AdminDashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: ""
+    }
+  }
+
+  componentDidMount() {
+    let token = localStorage.getItem('token')
+    axios.get("http://localhost:3000/api/v1/admin/dashboard?token=" + token)
+    .then((response) => {
+      this.setState({firstName: response.data.firstName})
+    })
+    .catch((error) => {
+      // TODO render a 404 page
+    })
+  }
+
   render() {
     return(
-      <div>This is the admin dashboard!</div>  
+      <div>Welcome, {this.state.firstName}!</div>
     )
   }
 }
--- src/StudentDashboard.js
+++ src/StudentDashboard.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import axios from 'axios';
 
 class StudentDashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: ""
+    }
+  }
+
+  componentDidMount() {
+    let token = localStorage.getItem('token')
+    axios.get("http://localhost:3000/api/v1/student/dashboard?token=" + token)
+    .then((response) => {
+      this.setState({firstName: response.data.firstName})
+    })
+    .catch((error) => {
+      // TODO render a 404 page
+    })
+  }
+
   render() {
     return(
-      <div>This is the student dashboard!</div>
+      <div>Welcome, {this.state.firstName}!</div>
     )
   }
 }
